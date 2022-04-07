479ce5f874ad27c0552c0b37ba9c569dd9856038
Maksim Tulenkov <maksozavr2003@gmail.com>
2022-04-07 21:06:06 +0000

    eight-ai

---
+++ js-ai/explored.js
@@ -0,0 +1,11 @@
+export class Explored extends Object
+{
+    add(state)
+    {
+        this[state.toString()] = true
+    }
+    hasState(state)
+    {
+        return Boolean(this[state.toString()])
+    }
+}
